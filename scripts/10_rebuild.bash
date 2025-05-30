d16fd4de6e1a690d0993eb2a740716d9fc3b448a
Talha Karabulut <talha-m-karabulut@hotmail.com>
2025-05-30 14:48:34 +0200

    chore: delete unnecessary comments

--- scripts/10_rebuild.bash
+++ scripts/10_rebuild.bash
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 cd /home/navigation_ws
-# source ros 2 distro
+
 source /opt/ros/humble/setup.bash
-# rosdep update --rosdistro humble
-# rosdep install --from-path src --ignore-src -r -y
-# build colcon ws
-# enforce clean build
+
 read -p "Do you wish to perform a clean installation? [y/n] " yn
 case $yn in
     [Yy]* ) bash /home/navigation_ws/scripts/01_clear_workspace.bash;; # remove any files from a previous build
