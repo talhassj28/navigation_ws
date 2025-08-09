a9290e42543b819c758c448ec4a37db4dd6dfb83
Talha Karabulut <talha-m-karabulut@hotmail.com>
2025-08-09 16:33:22 +0200

    feat: script to source gazebo environment

---
+++ scripts/set_gz_env.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+export GZ_SIM_RESOURCE_PATH=/home/navigation_ws/src:/home/navigation_ws/src/isa_afa_3/meshes:/home/navigation_ws/src/isa_afa_3/models:/home/navigation_ws/src/isa_afa_3/worlds:$GZ_SIM_RESOURCE_PATH
